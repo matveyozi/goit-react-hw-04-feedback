c62aca7aaf842a50d014658e6feda77d6064cd04
Matviy <antipin.m9971@gmail.com>
2023-05-23 20:24:55 +0300

    fix bag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,7 @@ export const App = () => {
   return (
     <div>
       <Feedback
-        initialValueGood={0}
-        initialValueNeutral={0}
-        initialValueBad={0}
+        
       />
     </div>
   );
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './Feedback.styled';
 import { FeedbackOptions } from './FeedbackOptions';
@@ -6,78 +6,65 @@ import { Statistics } from '../Statistics/Statistics';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 
-class Feedback extends Component {
+function Feedback() {
 
-	static defaultProps = {
-		initialValueGood: 0,
-		initialValueNeutral: 0,
-		initialValueBad: 0,
-	};
-	
-	state = {
-		good: this.props.initialValueGood,
-		neutral: this.props.initialValueNeutral,
-		bad: this.props.initialValueBad,
-	};
+const [good, setGood] = useState(0)
+const [neutral, setNeutral] = useState(0)
+const [bad, setBad] = useState(0)
 
-	onLeaveFeedback = ev => {
-		console.log(ev);
-		if (ev.target.value === 'good') {
-			this.setState(prevState => {
-				return {
-					good: prevState.good + 1,
-				};
-			});
+
+	const onLeaveFeedback = e => {
+		const { value } = e.target
+		
+		switch (value) {
+			case 'good': setGood(prev=> prev+1);
+				break;
+			case 'neutral': setNeutral(prev => prev+1);
+				break;
+			case 'bad': setBad(prev => prev+1);
+				break;
+			default: break;
 		}
-		if (ev.target.value === 'neutral') {
-			this.setState(prevState => {
-				return {
-					neutral: prevState.neutral + 1,
-				};
-			});
-		}
-		if (ev.target.value === 'bad') {
-			this.setState(prevState => {
-				return {
-					bad: prevState.bad + 1,
-				};
-			});
-		}
+
+		
+		
 	};
 
-	calculateTotal = () => {
-		let total = this.state.good + this.state.neutral + this.state.bad;
-		return total;
+	const calculateTotal = () => {
+		const totalResult = bad + good + neutral;
+		return totalResult;
 	};
-	calculatePositivePercentage = () => {
-		let percentage = (this.state.good / this.calculateTotal()) * 100;
+	const calculatePositivePercentage = () => {
+		let percentage = (good / calculateTotal()) * 100;
 		return Number.parseInt(percentage);
 	};
-	render() {
+	
+
+
 		return (
 			<Container>
 				<Section title="Please leave feedback">
 					<FeedbackOptions
 						options={['good', 'neutral', 'bad']}
-						onLeaveFeedback={this.onLeaveFeedback}
+						onLeaveFeedback={onLeaveFeedback}
 					/>
 				</Section>
 				<Section title="Statistics">
-					{this.calculateTotal() === 0 ? (
+					{calculateTotal() === 0 ? (
 						<Notification message="There is no feedback" />
 					) : (
 						<Statistics
-							good={this.state.good}
-							neutral={this.state.neutral}
-							bad={this.state.bad}
-							total={this.calculateTotal()}
-							positivePercentage={this.calculatePositivePercentage()}
+							good={good}
+							neutral={neutral}
+							bad={bad}
+							total={calculateTotal()}
+							positivePercentage={calculatePositivePercentage()}
 						></Statistics>
 					)}
 				</Section>
 			</Container>
 		);
-	}
+	
 }
 
 Feedback.propTypes = {
@@ -86,3 +73,88 @@ Feedback.propTypes = {
 	initialValueBad: PropTypes.number,
 };
 export default Feedback;
+
+
+// class Feedback extends Component {
+
+
+
+
+// 	static defaultProps = {
+// 		initialValueGood: 0,
+// 		initialValueNeutral: 0,
+// 		initialValueBad: 0,
+// 	};
+	
+// 	state = {
+// 		good: this.props.initialValueGood,
+// 		neutral: this.props.initialValueNeutral,
+// 		bad: this.props.initialValueBad,
+// 	};
+
+// 	onLeaveFeedback = ev => {
+// 		console.log(ev);
+// 		if (ev.target.value === 'good') {
+// 			this.setState(prevState => {
+// 				return {
+// 					good: prevState.good + 1,
+// 				};
+// 			});
+// 		}
+// 		if (ev.target.value === 'neutral') {
+// 			this.setState(prevState => {
+// 				return {
+// 					neutral: prevState.neutral + 1,
+// 				};
+// 			});
+// 		}
+// 		if (ev.target.value === 'bad') {
+// 			this.setState(prevState => {
+// 				return {
+// 					bad: prevState.bad + 1,
+// 				};
+// 			});
+// 		}
+// 	};
+
+// 	calculateTotal = () => {
+// 		let total = this.state.good + this.state.neutral + this.state.bad;
+// 		return total;
+// 	};
+// 	calculatePositivePercentage = () => {
+// 		let percentage = (this.state.good / this.calculateTotal()) * 100;
+// 		return Number.parseInt(percentage);
+// 	};
+// 	render() {
+// 		return (
+// 			<Container>
+// 				<Section title="Please leave feedback">
+// 					<FeedbackOptions
+// 						options={['good', 'neutral', 'bad']}
+// 						onLeaveFeedback={this.onLeaveFeedback}
+// 					/>
+// 				</Section>
+// 				<Section title="Statistics">
+// 					{this.calculateTotal() === 0 ? (
+// 						<Notification message="There is no feedback" />
+// 					) : (
+// 						<Statistics
+// 							good={this.state.good}
+// 							neutral={this.state.neutral}
+// 							bad={this.state.bad}
+// 							total={this.calculateTotal()}
+// 							positivePercentage={this.calculatePositivePercentage()}
+// 						></Statistics>
+// 					)}
+// 				</Section>
+// 			</Container>
+// 		);
+// 	}
+// }
+
+// Feedback.propTypes = {
+// 	initialValueGood: PropTypes.number,
+// 	initialValueNeutral: PropTypes.number,
+// 	initialValueBad: PropTypes.number,
+// };
+// export default Feedback;
